1ee2789f97b93d6914656d2f70ae248fd6476b25
SiPatron98 <patrykluczak@vp.pl>
2024-03-23 19:31:46 +0100

    added classes for this facade and unit tests

--- src/main/java/pl/lotto/domain/numberreceiver/dto/TicketDto.java
+++ src/main/java/pl/lotto/domain/numberreceiver/dto/TicketDto.java
@@ -6,5 +6,5 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 @Builder
-public record TicketDto(String hash, Set<Integer> numbers, LocalDateTime drawDate) {
+public record TicketDto(String hash, Set<Integer> numbers, LocalDateTime drawDate, String message, Integer age) {
 }
