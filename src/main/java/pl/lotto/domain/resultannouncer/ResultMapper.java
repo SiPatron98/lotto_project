caea6835e431efbdafe1730d380e81e74feb04dc
SiPatron98 <patrykluczak@vp.pl>
2024-03-24 17:13:26 +0100

    added classes for this facade and unit tests

---
+++ src/main/java/pl/lotto/domain/resultannouncer/ResultMapper.java
@@ -0,0 +1,16 @@
+package pl.lotto.domain.resultannouncer;
+
+import pl.lotto.domain.resultannouncer.dto.ResponseDto;
+
+public class ResultMapper {
+
+    static ResponseDto mapToDto(ResultResponse resultResponse) {
+        return ResponseDto.builder()
+                .hash(resultResponse.hash())
+                .numbers(resultResponse.numbers())
+                .hitNumbers(resultResponse.hitNumbers())
+                .drawDate(resultResponse.drawDate())
+                .isWinner(resultResponse.isWinner())
+                .build();
+    }
+}
---
+++ src/main/java/pl/lotto/domain/resultannouncer/dto/ResultAnnouncerResponseDto.java
@@ -0,0 +1,7 @@
+package pl.lotto.domain.resultannouncer.dto;
+
+import lombok.Builder;
+
+@Builder
+public record ResultAnnouncerResponseDto(ResponseDto responseDto, String message) {
+}
